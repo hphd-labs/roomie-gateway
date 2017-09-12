c153cb3e45435cfa895cf04da8cf684b6dd0977e
Andrew Burian <andrew@axiomzen.co>
2017-09-12 14:22:38 -0700

    Cleaner config

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"github.com/Sirupsen/logrus"
 	"github.com/andrewburian/powermux"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"github.com/Sirupsen/logrus"
-	"net/http"
 )
 
 const (
 	ROUTE_AUTH = "auth"
+)
+
+const (
+	CONF_PORT          = "PORT"
+	CONF_AUTH_UPSTREAM = "AUTH_UPSTREAM"
 )
 
 func main() {
@@ -21,21 +26,20 @@ func main() {
 	// The auth gateway
 	var authGateway *httputil.ReverseProxy
 	{
-		authUpstreamPath := os.Getenv("AUTH_UPSTREAM")
+		authUpstreamPath := os.Getenv(CONF_AUTH_UPSTREAM)
 		authUpstream, err := url.Parse(authUpstreamPath)
 		if err != nil {
-			logger.WithField("auth_upstream", authUpstreamPath).Fatal("Invalid Auth Upstream")
+			logger.WithField(CONF_AUTH_UPSTREAM, authUpstreamPath).Fatal("Invalid Auth Upstream")
 		}
 		authGateway = httputil.NewSingleHostReverseProxy(authUpstream)
 	}
-
 
 	// Setup the router
 	mux := powermux.NewServeMux()
 	mux.Route(ROUTE_AUTH).Any(authGateway)
 
 	// Run the server
-	port := os.Getenv("PORT")
+	port := os.Getenv(CONF_PORT)
 	if port == "" {
 		port = "http"
 	}
